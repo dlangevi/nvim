6b1a70860de085959a7fc3d4308a313514369930
David Langevin <dlangevi@uwaterloo.ca>
2023-10-31 04:09:26 -0700

    keep nvim tree size bigger if i set it

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,42 +1,41 @@
-
 return
 -- filebrowsing
 {
-    'nvim-tree/nvim-tree.lua',
-    -- dependencies = { 'nvim-tree/nvim-web-devicons' },
-    version = 'nightly', -- optional, updated every week. (see issue #1193)
-    config = function()
-      local nvimtree = require('nvim-tree')
-      local api = require('nvim-tree.api')
-      vim.g.nvim_tree_show_icons = {
-        folders = 0,
-        files = 0,
-        git = 0,
-        folder_arrows = 0,
+  'nvim-tree/nvim-tree.lua',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  version = 'nightly',   -- optional, updated every week. (see issue #1193)
+  config = function()
+    local nvimtree = require('nvim-tree')
+    local api = require('nvim-tree.api')
+    vim.g.nvim_tree_show_icons = {
+      folders = 0,
+      files = 0,
+      git = 0,
+      folder_arrows = 0,
+    }
+
+    nvimtree.setup({
+      view = {
+        side = "right",
+        preserve_window_proportions = true,
+      },
+      actions = {
+        open_file = {
+          resize_window = false,
+          window_picker = {
+            chars = "ASDFGHJKL"
+          },
+        },
+      },
+    })
+
+    require('keymapper').register({
+      d = {
+        name = "nvim-tree",
+        d = {
+          api.tree.toggle, "Toggle NvimTree"
+        },
       }
-
-      nvimtree.setup({
-          view = {
-              side = "right",
-              preserve_window_proportions = true,
-          },
-          actions = {
-              open_file = {
-                  window_picker = {
-                      chars = "ASDFGHJKL"
-                  },
-              },
-          },
-          
-      })
-
-      require('keymapper').register({
-          d = {
-              name = "nvim-tree",
-              d = {
-                  api.tree.toggle, "Toggle NvimTree"
-              },
-          }
-      }, { prefix = "<leader>" })
-    end,
+    }, { prefix = "<leader>" })
+  end,
 }
